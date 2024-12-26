e24266bd3f967ba764048d2ccc3553d520ac93ef
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-26 15:21:48 +0700

    U(ListTestPage) Chỉnh sửa cách hiển thị danh sách bài kiểm tra

--- src/features/listTest/pages/ListTestPage.tsx
+++ src/features/listTest/pages/ListTestPage.tsx
@@ -1,7 +1,18 @@
 import { Box } from "@mui/material";
-import { useParams } from "react-router-dom";
 
+import {  useParams } from "react-router-dom";
+import { MainPictureSection } from "components/sections";
+import useListTestPage from "../hooks/useListTestPage"
+import ListTest from "../components/ListTest";
 export default function ListTestPage() {
-  const { type } = useParams();
-  return <Box sx={{ mt: 8 }}>List test {type}</Box>;
+  const type = useParams<{ type?: string }>().type || "";
+
+  const listTestPage = useListTestPage();
+  return (
+    <Box sx={{ mt: 8 }}>
+        <MainPictureSection siteInfo={listTestPage.getSiteInfo(type)} />
+        <ListTest type={type} />
+    </Box>
+  );
+
 }
