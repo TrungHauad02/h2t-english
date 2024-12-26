571bed5860988014dcf3503f7973093bfe089cca
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-26 15:17:26 +0700

    U(ListTestServices) chỉnh sửa cách lấy dữ liệu theo loại bài kiểm tra

--- src/features/listTest/services/listTestServices.ts
+++ src/features/listTest/services/listTestServices.ts
@@ -2,8 +2,21 @@ import { mockData } from "./mockData";
 
 const getListTestByType = (type: string) => {
     
-    return mockData.tests.filter((test) => test.type === type.toUpperCase());
+    switch (type) {
+        case "mixing":
+          return mockData.tests.filter((test) => test.type === "MIXING");
+        case "readings":
+          return mockData.tests.filter((test) => test.type === "READING");
+        case "speakings":
+          return mockData.tests.filter((test) => test.type === "SPEAKING");
+        case "listenings":
+          return mockData.tests.filter((test) => test.type === "LISTENING");
+        case "writings":
+            return mockData.tests.filter((test) => test.type === "WRITING");
+      }
 };
+
+
 
 export const listTestService = {
     getListTestByType,
