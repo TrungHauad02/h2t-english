b658c21c63badd61e32f33efec628beec2d19e4f
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-26 13:51:11 +0700

    U(TestInterfaces): Thêm các trường cần thiết vào interface Test

--- src/interfaces/TestInterfaces.ts
+++ src/interfaces/TestInterfaces.ts
@@ -1,1 +1,11 @@
-export interface Test {}
+type TestTypeEnum = "MIXING" | "LISTENING" | "READING"| "SPEAKING"| "WRITING";
+export interface Test {
+  id: string;
+  title: string;
+  serial: number;
+  duration: number;
+  totalQuestions: number;
+  scoreLastOfTest: number | null;
+  type: TestTypeEnum;
+  status: boolean;
+}
