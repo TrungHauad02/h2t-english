5a59565a00b617717aa441a026461ac0a00e62be
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-01-06 16:07:14 +0700

    U(LessonPage): Khong truyen lesson vao TopicLesson

--- src/features/lesson/components/topic/TopicLesson.tsx
+++ src/features/lesson/components/topic/TopicLesson.tsx
@@ -1,11 +1,10 @@
-import { Topic } from "interfaces";
 import MatchImageWordSection from "./imageWord/MatchImageWordSection";
 import FlashcardSection from "./flashcard/FlashcardSection";
 import { CollapsibleSection } from "components/sections";
 import { Stack } from "@mui/material";
 import { AnswerQuestionSection } from "../common";
 
-export default function TopicLesson({ lesson }: { lesson: Topic }) {
+export default function TopicLesson() {
   return (
     <Stack sx={{ width: "100%" }}>
       <CollapsibleSection text="Match Image With Word">
