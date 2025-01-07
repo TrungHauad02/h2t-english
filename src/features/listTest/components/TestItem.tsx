c75a8592656fa076b1ff732ce7159d17e4e24046
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2025-01-07 11:24:15 +0700

    U(TestITem),C(TestItemStyles): Tách Style TestItem ra

--- src/features/listTest/components/TestItem.tsx
+++ src/features/listTest/components/TestItem.tsx
@@ -8,91 +8,37 @@ import {
   Button,
 } from "@mui/material";
 import { useState } from "react";
-import useColor from "theme/useColor";
-import { useDarkMode } from "hooks/useDarkMode";
-
+import { useTestItemStyles } from "./TestItemStyles";
+import { useNavigate } from "react-router-dom";
 export default function TestItem({ test }: { test: Test }) {
-  const { isDarkMode } = useDarkMode();
-  const color = useColor();
   const [hovered, setHovered] = useState(false);
+  const navigate = useNavigate();
+  const { cardStyles, cardContentStyles, typographyStyles, buttonStyles } =
+    useTestItemStyles(hovered);
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card
-        sx={{
-          maxWidth: 400,
-          margin: "16px auto",
-          boxShadow: hovered ? 6 : 3,
-          borderRadius: 2,
-          overflow: "hidden",
-          transition: "box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out",
-          transform: hovered ? "scale(1.05)" : "scale(1)",
-          cursor: "pointer",
-          backgroundColor: isDarkMode
-            ? hovered
-              ? color.gray800
-              : color.gray900
-            : hovered
-            ? color.teal50
-            : color.gray50,
-        }}
+        sx={cardStyles}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
       >
-        <CardContent
-          sx={{
-            backgroundColor: isDarkMode
-              ? hovered
-                ? color.gray700
-                : color.gray800
-              : hovered
-              ? color.teal100 + "40"
-              : color.gray100,
-            transition: "background-color 0.3s ease-in-out",
-          }}
-        >
+        <CardContent sx={cardContentStyles}>
           <Typography
             variant="h6"
             component="div"
             gutterBottom
-            sx={{
-              color: isDarkMode
-                ? hovered
-                  ? color.teal400
-                  : color.teal200
-                : hovered
-                ? color.teal700
-                : color.gray800,
-              transition: "color 0.3s ease-in-out",
-            }}
+            sx={typographyStyles.title}
           >
             {test?.title}
           </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              color: isDarkMode ? color.gray400 : color.gray600,
-            }}
-            paragraph
-          >
+          <Typography variant="body2" sx={typographyStyles.body} paragraph>
             Duration: {test?.duration} minutes
           </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              color: isDarkMode ? color.gray400 : color.gray600,
-            }}
-            paragraph
-          >
+          <Typography variant="body2" sx={typographyStyles.body} paragraph>
             Total question: {test?.totalQuestions}
           </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              color: isDarkMode ? color.gray400 : color.gray600,
-            }}
-            paragraph
-          >
+          <Typography variant="body2" sx={typographyStyles.body} paragraph>
             Score: {test?.scoreLastOfTest || 0}
           </Typography>
           <Box
@@ -102,41 +48,9 @@ export default function TestItem({ test }: { test: Test }) {
               alignItems: "center",
             }}
           >
-            <Button
-              sx={{
-                padding: 0,
-                color: isDarkMode ? color.teal300 : color.teal700,
-                borderColor: isDarkMode ? color.teal300 : color.teal700,
-                textDecoration: "underline",
-                borderRadius: "99rem",
-                marginRight: "auto",
-                ":hover": {
-                  bgcolor: isDarkMode
-                    ? hovered
-                      ? color.gray700
-                      : color.gray800
-                    : hovered
-                    ? color.teal100
-                    : color.gray100,
-                },
-              }}
-            >
-              See History
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                padding: "0.5rem 1rem",
-                backgroundColor: isDarkMode ? color.teal700 : color.teal600,
-                color: color.white,
-                borderRadius: "0.5rem",
-                transition: "0.3s ease-in-out",
-                marginLeft: "auto",
-                ":hover": {
-                  backgroundColor: isDarkMode ? color.teal600 : color.teal500,
-                },
-              }}
-            >
+            <Button sx={buttonStyles.seeHistory}>See History</Button>
+            <Button variant="contained" sx={buttonStyles.doTest}
+            onClick={() => navigate(`${test.id}`)}>
               Do Test
             </Button>
           </Box>
