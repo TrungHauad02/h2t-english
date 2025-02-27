d00d758d6c203ec8a253965a66e32dccaa4c1fb4
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-02-27 15:05:59 +0700

    U(ReadingLesson): Hoan thanh reading lesson

--- src/features/lesson/components/reading/ReadingLesson.tsx
+++ src/features/lesson/components/reading/ReadingLesson.tsx
@@ -2,12 +2,24 @@ import { Reading } from "interfaces";
 import { Box } from "@mui/material";
 import { CollapsibleSection } from "components/sections";
 import PreparationSection from "../common/preparation/PreparationSection";
+import WEDocumentViewer from "components/display/document/WEDocumentViewer";
+import { AnswerQuestionSection } from "../common";
 
 export default function ReadingLesson({ lesson }: { lesson: Reading }) {
   return (
     <Box sx={{ mx: 8, my: 4 }}>
       <CollapsibleSection text="Reading Preparation">
         <PreparationSection />
+      </CollapsibleSection>
+      <CollapsibleSection text="Reading Document">
+        <WEDocumentViewer
+          fileUrl={"/document.docx"}
+          lineHeight="2"
+          sx={{ my: 2 }}
+        />
+      </CollapsibleSection>
+      <CollapsibleSection text="Reading Quiz">
+        <AnswerQuestionSection />
       </CollapsibleSection>
     </Box>
   );
