af571613323fab9cc485ac3a0ff9c77ca4c30aa1
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-03-23 15:08:35 +0700

    R(HeroSection)

---
+++ src/features/home/components/hero/index.ts
@@ -0,0 +1,5 @@
+export { default as HeroSectionButtons } from "./HeroSectionButtons";
+export { default as HeroSectionImage } from "./HeroSectionImage";
+export { default as HeroSectionInfo } from "./HeroSectionInfo";
+export { default as HeroSectionTitle } from "./HeroSectionTitle";
+export { default as useHeroSection } from "./useHeroSection";
--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,13 +1,9 @@
 export { default as CompetitionsSection } from "./CompetitionsSection";
-export { default as FeaturesSection } from "./featuresSection/FeaturesSection";
-export { default as HeroSection } from "./heroSection/HeroSection";
+export { default as FeaturesSection } from "./FeaturesSection";
 export { default as LeaderboardSection } from "./LeaderboardSection";
 export { default as LessonsSection } from "./LessonsSection";
 export { default as RoutesSection } from "./RoutesSection";
 export { default as TestimonialsSection } from "./TestimonialsSection";
 export { default as TestsSection } from "./TestsSection";
 export { default as ToeicSection } from "./ToeicSection";
-export { default as HeroSectionTitle } from "./heroSection/HeroSectionTitle";
-export { default as HeroSectionButtons } from "./heroSection/HeroSectionButtons";
-export { default as HeroSectionImage } from "./heroSection/HeroSectionImage";
-export { default as HeroSectionInfo } from "./heroSection/HeroSectionInfo";
+export { default as HeroSection } from "./HeroSection";
