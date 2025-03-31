ab7ee975eae0779424869c501afd49b7a861344f
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2025-03-31 14:53:42 +0700

    U(Toeic): cap nhat lai full man hinh cho trang kiem tra toeic

--- src/layouts/StudentLayout.tsx
+++ src/layouts/StudentLayout.tsx
@@ -1,29 +1,32 @@
-import { Outlet } from "react-router-dom";
-import StudentHeader from "./header/StudentHeader";
-import Footer from "./footer/Footer";
-import { Box } from "@mui/material";
-import useColor from "theme/useColor";
-import { useDarkMode } from "hooks/useDarkMode";
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import StudentHeader from './header/StudentHeader';
+import Footer from './footer/Footer';
+import { Box } from '@mui/material';
+import useColor from 'theme/useColor';
+import { useDarkMode } from 'hooks/useDarkMode';
 
 export default function StudentLayout() {
   const { isDarkMode } = useDarkMode();
   const color = useColor();
+  const showLayout = useSelector((state: any) => state.layout.showHeaderFooter);
+
   return (
     <Box
       sx={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
         px: { xs: 2, sm: 4 },
         bgcolor: isDarkMode ? color.gray900 : color.gray100,
         color: isDarkMode ? color.white : color.black,
       }}
     >
-      <StudentHeader />
+      {showLayout && <StudentHeader />}
       <Outlet />
-      <Footer />
+      {showLayout && <Footer />}
     </Box>
   );
 }
