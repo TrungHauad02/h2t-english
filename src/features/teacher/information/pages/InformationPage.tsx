5af202c68f21563cd7507ad76d38b71f06c01c20
hung <hungat418@gmail.com>
2025-04-01 23:30:34 +0700

    C(TeacherInformation): them infor cho teacher

--- src/features/teacher/information/pages/InformationPage.tsx
+++ src/features/teacher/information/pages/InformationPage.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
-
+import TeacherInformation from "../components/TeacherInformation";
 export default function InformationPage() {
-  return <Box sx={{ mt: 6 }}>InformationPage</Box>;
+  return <Box sx={{ mt: 8 }}>
+    <TeacherInformation/>
+  </Box>;
 }
