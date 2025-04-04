990ed05663aa0a2c5fef6ca5084df2565fca1a15
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-04-04 16:00:45 +0700

    U(ListeningDetailPage): Ket noi api thong tin co ban

--- src/features/teacher/manageLesson/hooks/useListeningDetailPage.ts
+++ src/features/teacher/manageLesson/hooks/useListeningDetailPage.ts
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { Listening } from "interfaces";
-import { listLessonService } from "../services/listLessonService";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { listeningService, routeService } from "services";
+import { extractErrorMessages } from "utils/extractErrorMessages";
+import { useErrors } from "hooks/useErrors";
 
 export default function useListeningDetailPage() {
   const { id, routeId } = useParams();
@@ -12,19 +14,41 @@ export default function useListeningDetailPage() {
   const [openPublishDialog, setOpenPublishDialog] = useState<boolean>(false);
   const [openUnpublishDialog, setOpenUnpublishDialog] =
     useState<boolean>(false);
+  const { showError } = useErrors();
 
   useEffect(() => {
-    if (id && routeId) {
-      setLoading(true);
-      setTimeout(() => {
-        const listening = listLessonService.getListeningById(parseInt(id));
-        if (listening) {
-          setData(listening);
-          setEditData({ ...listening });
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        if (id && routeId) {
+          //  Check owner id using routeId
+          const resRouteData = await routeService.getRouteById(
+            parseInt(routeId)
+          );
+          if (resRouteData.data) {
+            if (resRouteData.data.ownerId !== 1) {
+              // TODO: Check with real teacher ID
+              // TODO: Display error
+              return;
+            }
+          }
+          const resData = await listeningService.findById(parseInt(id));
+          if (resData.data) {
+            setData(resData.data);
+            setEditData({ ...resData.data });
+          }
+          setLoading(false);
         }
-        setLoading(false);
-      }, 500);
-    }
+      } catch (error) {
+        // Display error
+        showError({
+          message: "Error fetching listening",
+          severity: "error",
+          details: extractErrorMessages(error),
+        });
+      }
+    };
+    fetchData();
   }, [id, routeId]);
 
   const handleEditMode = () => {
@@ -34,11 +58,22 @@ export default function useListeningDetailPage() {
     }
   };
 
-  const handleSaveChanges = () => {
-    if (editData) {
-      setData(editData);
+  const handleSaveChanges = async () => {
+    try {
+      if (editData) {
+        const resData = await listeningService.update(editData.id, editData);
+        setData(resData.data);
+      }
+    } catch (error) {
+      console.error("Error updating listening");
+      // Display error
+      showError({
+        message: "Error updating listening",
+        severity: "error",
+        details: extractErrorMessages(error),
+      });
+    } finally {
       setIsEditMode(false);
-      // TODO: Update in db
     }
   };
 
@@ -51,23 +86,46 @@ export default function useListeningDetailPage() {
     }
   };
 
-  const handlePublish = () => {
-    if (data) {
-      const updatedData = { ...data, status: true };
-      setData(updatedData);
-      // TODO: Update in db
-      if (editData) setEditData(updatedData);
-      setOpenPublishDialog(false);
+  const handlePublish = async () => {
+    try {
+      if (data) {
+        // TODO: Check valid lesson before publish
+        const resData = await listeningService.patch(data.id, {
+          status: true,
+        });
+        setData(resData.data);
+        if (editData) setEditData(resData.data);
+        setOpenPublishDialog(false);
+      }
+    } catch (error) {
+      console.error("Error publishing listening");
+      // Display error
+      showError({
+        message: "Error publishing listening",
+        severity: "error",
+        details: extractErrorMessages(error),
+      });
     }
   };
 
-  const handleUnpublish = () => {
-    if (data) {
-      const updatedData = { ...data, status: false };
-      setData(updatedData);
-      // TODO: Update in db
-      if (editData) setEditData(updatedData);
-      setOpenUnpublishDialog(false);
+  const handleUnpublish = async () => {
+    try {
+      if (data) {
+        const resData = await listeningService.patch(data.id, {
+          status: false,
+        });
+        setData(resData.data);
+        if (editData) setEditData(resData.data);
+        setOpenUnpublishDialog(false);
+      }
+    } catch (error) {
+      console.error("Error unPublishing listening");
+      // Display error
+      showError({
+        message: "Error unPublishing listening",
+        severity: "error",
+        details: extractErrorMessages(error),
+      });
     }
   };
 
