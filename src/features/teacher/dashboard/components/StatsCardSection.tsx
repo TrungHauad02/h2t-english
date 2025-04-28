4d13e941ddbeafeff7e7405108a574f060f2282a
Nguyen Trung Hau <trunghauad02@gmail.com>
2025-04-28 12:16:29 +0700

    U(StatsCard): Xoa changePercentage

--- src/features/teacher/dashboard/components/StatsCardSection.tsx
+++ src/features/teacher/dashboard/components/StatsCardSection.tsx
@@ -32,7 +32,6 @@ export default function StatsCardSection({
           title="Number of routes"
           value={totalRoutes}
           bgColor={isDarkMode ? color.teal900 : color.teal100}
-          changePercentage={12}
         />
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
@@ -41,7 +40,6 @@ export default function StatsCardSection({
           title="Number of lessons"
           value={numberLessons}
           bgColor={isDarkMode ? color.emerald900 : color.emerald100}
-          changePercentage={8}
         />
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
@@ -50,7 +48,6 @@ export default function StatsCardSection({
           title="Number of tests"
           value={totalTests}
           bgColor={isDarkMode ? color.green900 : color.green100}
-          changePercentage={15}
         />
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
@@ -59,7 +56,6 @@ export default function StatsCardSection({
           title="Views this month"
           value={totalViews}
           bgColor={isDarkMode ? color.green900 : color.green100}
-          changePercentage={-12}
         />
       </Grid>
     </Grid>
